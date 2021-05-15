a1372c85d420bdc586d1a2986a55147a408630c9
Inseok Lee <git@dlun.ch>
2021-05-15 09:42:28 +0900

    Implement rtsp server skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod mdns;
+mod rtsp;
+
+use std::error::Error;
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    rtsp::serve("0.0.0.0".parse()?, 7000).await?;
+
+    Ok(())
 }
