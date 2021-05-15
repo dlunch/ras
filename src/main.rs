ff2627a53209011dd860c11d6de3499923cfe196
Inseok Lee <git@dlun.ch>
2021-05-15 16:54:07 +0900

    Send response with sequence

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::error::Error;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+
     rtsp::serve("0.0.0.0".parse()?, 5000).await?;
 
     Ok(())
--- src/rtsp/mod.rs
+++ src/rtsp/mod.rs
@@ -6,6 +6,7 @@ use std::str;
 use async_std::io;
 use async_std::net::{IpAddr, SocketAddr, TcpListener};
 use async_std::stream::StreamExt;
+use maplit::hashmap;
 
 use request::Request;
 use response::{Response, StatusCode};
@@ -18,17 +19,23 @@ pub async fn serve(ip: IpAddr, port: u16) -> io::Result<()> {
     while let Some(stream) = incoming.next().await {
         let mut stream = stream?;
 
-        let req = Request::parse(&mut stream).await?;
+        loop {
+            let req = Request::parse(&mut stream).await?;
 
-        println!(
-            "{} {:?} {:?}",
-            req.method,
-            req.headers,
-            str::from_utf8(&req.content).unwrap()
-        );
+            println!(
+                "req {} {:?} {:?}",
+                req.method,
+                req.headers,
+                str::from_utf8(&req.content).unwrap()
+            );
 
-        let res = Response::new(StatusCode::Ok);
-        res.write(&mut stream).await?;
+            let res = Response::new(
+                StatusCode::Ok,
+                hashmap! {"CSeq".into() => req.headers.get("CSeq").unwrap().into()},
+            );
+
+            res.write(&mut stream).await?;
+        }
     }
 
     Ok(())
