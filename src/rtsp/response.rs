f3539be5e05af229207e96370aa4e5e916f3f2ea
Inseok Lee <git@dlun.ch>
2021-05-15 16:54:07 +0900

    Send response with sequence

--- src/rtsp/response.rs
+++ src/rtsp/response.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use async_std::io::{self, prelude::WriteExt};
 
 #[derive(Clone, Copy)]
@@ -18,11 +20,12 @@ impl StatusCode {
 
 pub struct Response {
     status: StatusCode,
+    headers: HashMap<String, String>,
 }
 
 impl Response {
-    pub fn new(status: StatusCode) -> Self {
-        Self { status }
+    pub fn new(status: StatusCode, headers: HashMap<String, String>) -> Self {
+        Self { status, headers }
     }
 
     pub async fn write<S>(&self, mut stream: S) -> io::Result<()>
@@ -30,12 +33,20 @@ impl Response {
         S: io::Write + Unpin,
     {
         let status_line = format!(
-            "RTSP/1.0 {} {}",
+            "RTSP/1.0 {} {}\r\n",
             self.status as usize,
             self.status.as_string()
         );
 
         stream.write(status_line.as_bytes()).await?;
+
+        for (key, value) in &self.headers {
+            let header_line = format!("{}: {}\r\n", key, value);
+
+            stream.write(header_line.as_bytes()).await?;
+        }
+
+        stream.write("\r\n".as_bytes());
 
         Ok(())
     }
