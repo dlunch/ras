658d0e4da612c134b8f55f60e23d1b9ca54bfcb5
Inseok Lee <git@dlun.ch>
2021-05-15 15:45:18 +0900

    Parse request

--- src/rtsp/mod.rs
+++ src/rtsp/mod.rs
@@ -1,6 +1,10 @@
-use async_std::io::{self, prelude::BufReadExt, BufReader};
+mod request;
+
+use async_std::io;
 use async_std::net::{IpAddr, SocketAddr, TcpListener};
 use async_std::stream::StreamExt;
+
+use request::Request;
 
 pub async fn serve(ip: IpAddr, port: u16) -> io::Result<()> {
     let listener = TcpListener::bind(SocketAddr::new(ip, port)).await?;
@@ -8,14 +12,11 @@ pub async fn serve(ip: IpAddr, port: u16) -> io::Result<()> {
     let mut incoming = listener.incoming();
 
     while let Some(stream) = incoming.next().await {
-        let stream = stream?;
-        let reader = BufReader::new(stream);
+        let mut stream = stream?;
 
-        let mut lines = reader.lines();
+        let req = Request::parse(&mut stream).await?;
 
-        while let Some(line) = lines.next().await {
-            println!("{}", line?);
-        }
+        println!("{} {:?} {:?}", req.method, req.headers, req.content);
     }
 
     Ok(())
