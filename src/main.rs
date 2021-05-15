6c60fa053b6d20686d47bd1e8728ccf6ea91a60a
Inseok Lee <git@dlun.ch>
2021-05-16 08:45:36 +0900

    mdns test

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,25 @@ mod rtsp;
 
 use std::error::Error;
 
+use async_std::net::{IpAddr, Ipv4Addr};
+use async_std::task::spawn;
+use futures::join;
+
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
 
-    rtsp::serve("0.0.0.0".parse()?, 5000).await?;
+    let rtsp_join_handle = spawn(async {
+        rtsp::serve(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 5000)
+            .await
+            .unwrap();
+    });
+
+    let mdns_join_handle = spawn(async {
+        mdns::serve("_raop._tcp").await.unwrap();
+    });
+
+    join!(rtsp_join_handle, mdns_join_handle);
 
     Ok(())
 }
