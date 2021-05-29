ca549a9fb1ae54827620766d25fb26ce08d05380
Inseok Lee <git@dlun.ch>
2021-05-29 17:54:17 +0900

    Split packet handling

--- src/mdns/mod.rs
+++ src/mdns/mod.rs
@@ -12,7 +12,14 @@ use multicast_socket::MulticastSocket;
 
 use packet::Packet;
 
-pub async fn serve(service_type: &str, service_name: &str, service_port: u16, txt: &[&'static str]) -> io::Result<()> {
+pub struct Service {
+    pub r#type: &'static str,
+    pub name: &'static str,
+    pub port: u16,
+    pub txt: Vec<&'static str>,
+}
+
+pub async fn serve(service: &Service) -> io::Result<()> {
     let mdns_addr = Ipv4Addr::new(224, 0, 0, 251);
 
     let socket = Arc::new(MulticastSocket::all_interfaces(SocketAddrV4::new(mdns_addr, 5353))?);
@@ -22,20 +29,26 @@ pub async fn serve(service_type: &str, service_name: &str, service_port: u16, tx
         let message = spawn_blocking(move || socket2.receive()).await?;
         debug!("receive from {}, raw {:?}", message.origin_address, message.data);
 
-        let packet = Packet::parse(&message.data);
-        if packet.header.is_query() {
-            for question in &packet.questions {
-                debug!("question {}", question.name);
+        let response = handle_packet(&message.data, &service);
 
-                if question.name.equals(service_type) {
-                    // TODO
-                }
+        if let Some(response) = response {
+            debug!("sending response to {:?}, raw {:?}", message.origin_address, response);
+            socket.send(&response, &message.interface)?;
+        }
+    }
+}
+
+fn handle_packet(data: &[u8], service: &Service) -> Option<Vec<u8>> {
+    let packet = Packet::parse(&data);
+    if packet.header.is_query() {
+        for question in &packet.questions {
+            debug!("question {}", question.name);
+
+            if question.name.equals(service.r#type) {
+                // TODO
             }
         }
-
-        let response = vec![0, 0];
-
-        debug!("sending response to {:?}, raw {:?}", message.origin_address, response);
-        socket.send(&response, &message.interface)?;
     }
+
+    Some(vec![0, 0])
 }
