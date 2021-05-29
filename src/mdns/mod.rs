c5980e3afb0c0813630bcb29c9e6ee1241f56571
Inseok Lee <git@dlun.ch>
2021-05-29 14:37:49 +0900

    Use socket2

--- src/mdns/mod.rs
+++ src/mdns/mod.rs
@@ -3,6 +3,7 @@ mod packet;
 use async_std::io;
 use async_std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 use log::trace;
+use socket2::{Domain, Protocol, SockAddr, Socket, Type};
 
 use packet::Packet;
 
@@ -10,12 +11,16 @@ pub async fn serve(service_type: &str, service_name: &str, service_port: u16, tx
     let any = Ipv4Addr::new(0, 0, 0, 0);
     let mdns_addr = Ipv4Addr::new(224, 0, 0, 251);
 
-    let socket = UdpSocket::bind(SocketAddr::new(IpAddr::V4(any), 5353)).await?;
-    socket.join_multicast_v4(mdns_addr, any)?;
+    let raw_socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
+    raw_socket.set_reuse_address(true)?;
+    raw_socket.bind(&SockAddr::from(SocketAddr::new(IpAddr::V4(any), 5353)))?;
+    raw_socket.join_multicast_v4(&mdns_addr, &any)?;
+
+    let socket = UdpSocket::from(std::net::UdpSocket::from(raw_socket.try_clone()?));
 
     loop {
         let mut buf = [0; 2048];
-        socket.recv_from(&mut buf).await?;
+        let (_, remote_addr) = socket.recv_from(&mut buf).await?;
 
         let packet = Packet::parse(&buf);
 
@@ -29,7 +34,10 @@ pub async fn serve(service_type: &str, service_name: &str, service_port: u16, tx
             }
         }
 
-        // we should set IP_MULTICAST_IF on multicast response
+        // set multicast response interface
+        if let IpAddr::V4(x) = remote_addr.ip() {
+            raw_socket.set_multicast_if_v4(&x)?;
+        }
     }
 
     Ok(())
