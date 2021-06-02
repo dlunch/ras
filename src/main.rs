ddeb6e1aa5b493ba330f2549951bdc33bed0c809
Inseok Lee <git@dlun.ch>
2021-06-02 17:07:20 +0900

    use libmdns

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-mod mdns;
 mod rtsp;
 
 use std::error::Error;
@@ -16,7 +15,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     });
 
     let mdns_join_handle = spawn(async {
-        mdns::serve("_raop._tcp").await.unwrap();
+        let txt = vec![
+            "am=AppleTV3,2",
+            "cn=0,1,2,3",
+            "da=true",
+            "et=0,3,5",
+            "md=0,1,2",
+            "sf=0x4",
+            "tp=UDP",
+            "vn=65537",
+            "vs=220.68",
+            "vv=2",
+        ];
+        let responder = libmdns::Responder::new().unwrap();
+        let _svc = responder.register("_raop._tcp".to_owned(), "test".to_owned(), 7000, &txt);
     });
 
     join!(rtsp_join_handle, mdns_join_handle);
