bd8cb5f259f9891ee88fee906cd298894a55bc08
Inseok Lee <git@dlun.ch>
2021-06-13 17:01:28 +0900

    Fix incorrect rtsp response

--- src/rtsp/response.rs
+++ src/rtsp/response.rs
@@ -42,8 +42,26 @@ impl Response {
             stream.write(header_line.as_bytes()).await?;
         }
 
-        stream.write("\r\n".as_bytes());
+        stream.write("\r\n".as_bytes()).await?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use maplit::hashmap;
+    use std::str;
+
+    #[async_std::test]
+    async fn test_simple_response() {
+        let response = Response::new(StatusCode::Ok, hashmap! { "Test".into() => "Test".into() });
+
+        let mut buf = Vec::new();
+        response.write(&mut buf).await.unwrap();
+
+        let response_text = str::from_utf8(&buf).unwrap();
+        assert_eq!(response_text, "RTSP/1.0 200 OK\r\nTest: Test\r\n\r\n");
     }
 }
