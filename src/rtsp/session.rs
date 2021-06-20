04f870ab15e280032c6c2ed2bb48b0df474603aa
Inseok Lee <git@dlun.ch>
2021-06-20 17:28:38 +0900

    Handle transport

--- src/rtsp/session.rs
+++ src/rtsp/session.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, io, str};
 
-use async_std::net::TcpStream;
-use log::warn;
+use async_std::net::{Ipv4Addr, SocketAddrV4, TcpStream, UdpSocket};
+use log::{debug, warn};
 use maplit::hashmap;
 
 use super::{
@@ -11,11 +11,19 @@ use super::{
 
 pub struct Session {
     stream: TcpStream,
+    rtp: Option<UdpSocket>,
+    control: Option<UdpSocket>,
+    timing: Option<UdpSocket>,
 }
 
 impl Session {
     pub async fn start(stream: TcpStream) -> io::Result<()> {
-        let mut session = Self { stream };
+        let mut session = Self {
+            stream,
+            rtp: None,
+            control: None,
+            timing: None,
+        };
 
         session.run().await
     }
@@ -36,7 +44,7 @@ impl Session {
                 str::from_utf8(&req.content).unwrap()
             );
 
-            let res = self.handle_request(&req);
+            let res = self.handle_request(&req).await?;
             println!("res {} {:?}", res.status as u32, res.headers);
 
             res.write(&mut self.stream).await?;
@@ -45,12 +53,12 @@ impl Session {
         Ok(())
     }
 
-    fn handle_request(&self, request: &Request) -> Response {
+    async fn handle_request(&mut self, request: &Request) -> io::Result<Response> {
         let cseq = request.headers.get("CSeq").unwrap();
 
         let (status, mut header) = match request.method.as_str() {
             "ANNOUNCE" => (StatusCode::Ok, HashMap::new()),
-            "SETUP" => self.setup(request),
+            "SETUP" => self.setup(request).await?,
             _ => {
                 warn!("Unhandled method {}", request.method);
 
@@ -60,16 +68,29 @@ impl Session {
 
         header.insert("CSeq", cseq.into());
 
-        Response::new(status, header)
+        Ok(Response::new(status, header))
     }
 
-    fn setup(&self, request: &Request) -> (StatusCode, HashMap<&'static str, String>) {
-        let transport = request.headers.get("Transport").unwrap();
+    async fn setup(&mut self, request: &Request) -> io::Result<(StatusCode, HashMap<&'static str, String>)> {
+        let client_transport = request.headers.get("Transport").unwrap();
+
+        debug!("client_transport: {:?}", client_transport);
+
+        self.rtp = Some(UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0)).await?);
+        self.control = Some(UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0)).await?);
+        self.timing = Some(UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0)).await?);
+
+        let transport = format!(
+            "RTP/AVP/UDP;unicast;mode=record;server_port={};control_port={};timing_port={}",
+            self.rtp.as_ref().unwrap().local_addr()?.port(),
+            self.control.as_ref().unwrap().local_addr()?.port(),
+            self.timing.as_ref().unwrap().local_addr()?.port()
+        );
 
         let response_headers = hashmap! {
-            "Transport" => transport.into()
+            "Transport" => transport
         };
 
-        (StatusCode::Ok, response_headers)
+        Ok((StatusCode::Ok, response_headers))
     }
 }
