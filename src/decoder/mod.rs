9c03f40a6092951a4fc1427e8d625aa61f223f8a
Inseok Lee <git@dlun.ch>
2021-06-26 09:21:59 +0900

    Add alac decoder

---
+++ src/decoder/mod.rs
@@ -0,0 +1,27 @@
+use alac::{Decoder as AlacDecoder, StreamInfo};
+
+pub trait Decoder {
+    fn decode(&mut self, raw: &[u8]) -> Vec<i32>;
+}
+
+pub struct AppleLoselessDecoder {
+    decoder: AlacDecoder,
+}
+
+impl AppleLoselessDecoder {
+    pub fn new(fmtp: &str) -> Self {
+        let stream_info = StreamInfo::from_sdp_format_parameters(fmtp).unwrap();
+        let decoder = AlacDecoder::new(stream_info);
+
+        Self { decoder }
+    }
+}
+
+impl Decoder for AppleLoselessDecoder {
+    fn decode(&mut self, raw: &[u8]) -> Vec<i32> {
+        let mut out = vec![0; self.decoder.stream_info().max_samples_per_packet() as usize];
+        self.decoder.decode_packet(raw, &mut out).unwrap();
+
+        out
+    }
+}
