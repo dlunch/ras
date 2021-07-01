cb8bb2f12ec2176eb3975c0cb51343cd8d19b530
Inseok Lee <git@dlun.ch>
2021-07-01 19:49:57 +0900

    Fix invalid convert

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,6 @@
 pub unsafe fn convert_vec<T, U>(mut src: Vec<T>) -> Vec<U> {
-    let ratio = std::mem::size_of::<T>() / std::mem::size_of::<U>();
-
-    let length = src.len() * ratio;
-    let capacity = src.capacity() * ratio;
+    let length = src.len() * std::mem::size_of::<T>() / std::mem::size_of::<U>();
+    let capacity = src.capacity() * std::mem::size_of::<T>() / std::mem::size_of::<U>();
     let ptr = src.as_mut_ptr() as *mut U;
 
     std::mem::forget(src);
