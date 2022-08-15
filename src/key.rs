5590f34420e53b4525d5bff7cb6fa5559d5305b6
Inseok Lee <git@dlun.ch>
2022-08-15 09:52:19 +0900

    Extract rtp decrypt codes

--- src/key.rs
+++
@@ -1,5 +0,0 @@
-use rsa::{pkcs1::DecodeRsaPrivateKey, RsaPrivateKey};
-
-lazy_static::lazy_static! {
-    pub static ref RTSP_KEY: RsaPrivateKey = RsaPrivateKey::from_pkcs1_pem(include_str!("rtsp.key")).unwrap();
-}
